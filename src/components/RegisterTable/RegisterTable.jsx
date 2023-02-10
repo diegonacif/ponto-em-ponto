75313fe714e264a6ac8cff9426f0efca6c3bddbb
Diego Nacif <diegoreisnacif@gmail.com>
2023-02-10 17:05:44 -0300

    feat: dinamic register table

--- src/components/RegisterTable/RegisterTable.jsx
+++ src/components/RegisterTable/RegisterTable.jsx
@@ -3,27 +3,45 @@ import '../../css/App.css';
 
 export const RegisterTable = () => {
   const date = new Date();
+  const dateHour = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 
-  const [dateTime, setDateTime] = useState({
-    hours: date.getHours(),
-    minutes: date.getMinutes(),
-    seconds: date.getSeconds()
-  });
+  const tableRow = [
+    date.toLocaleDateString(),
+    "tipo",
+    `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+  ]
+
+  const [tableData, setTableData] = useState([]);
+
+  console.log(tableData);
+
+  function addRow() {
+    setTableData([...tableData, ...tableRow])
+  }
 
   return (
     <div className="register-table-container">
       <table>
-        <thead>
+        <tr>
           <th>Data</th>
           <th>Tipo</th>
           <th>Hora</th>
-        </thead>
+        </tr>
         <tr>
+        {
+          tableData.map((data, key) => 
+            <td key={key}>{data}</td>
+          )
+        }
+        </tr>
+
+        {/* <tr>
           <td>10/02/2023</td>
           <td>Entrada manhã</td>
-          <td>{dateTime.hours}:{dateTime.minutes}:{dateTime.seconds}</td>
-        </tr>
+          <td>{dateHour}</td>
+        </tr> */}
       </table>
+      <button onClick={() => addRow()}>Add</button>
     </div>
   )
 }
